0b86ecac466b283e7c2298951df628f3f4b40a2f
Home iMac <homeimac@Homes-iMac.local>
2020-04-26 15:04:20 +0545

    Correction

--- renderScript.py
+++ renderScript.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python
-
 import sys
 import bpy
 
-def looop(start_frame, start_frame, location):
+def looop(start_frame, end_frame, location):
+    print('Came in looop')
     for step in range(start_frame, end_frame + 1):
         bpy.context.scene.frame_set(step)
         
-        bpy.data.scenes["Scene"].render.filepath = location + '\V.png_%d.png' % step
+        bpy.data.scenes["Scene"].render.filepath = location + '/V.png_%d.png' % step
 
         bpy.ops.render.render( write_still=True )
 
@@ -16,9 +15,15 @@ def looop(start_frame, start_frame, location):
 argv = sys.argv
 list_of_arg = argv[argv.index("--") + 1:]  # get all args after "--"
 
-list_of_arg = str(sys.argv)
+list_of_arg = list(sys.argv)
 
-location = str(list_of_arg[0])
-start_frame = int(list_of_arg[1])
-start_frame = int(list_of_arg[2])
-looop(start_frame, start_frame, location)
+print(list_of_arg )
+location = str(list_of_arg[-1])
+print('location: ' , location )
+end_frame = int(list_of_arg[-2])
+print('end_frame  : ' , end_frame )
+start_frame = int(list_of_arg[-3])
+print('start_frame   : ' , start_frame )
+
+
+looop(start_frame, end_frame, location)
